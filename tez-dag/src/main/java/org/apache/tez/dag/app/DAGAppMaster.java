81a02833cae243a5d2d9b74b21dcfaae38b1b546
Jonathan Eagles <jeagles@gmail.com>
2014-08-22 17:08:59 -0500

    TEZ-1485. Disable node blacklisting and ATS in AM for local mode (jeagles)

--- tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
+++ tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
@@ -299,6 +299,9 @@ public class DAGAppMaster extends AbstractService {
     if (isLocal) {
        UserGroupInformation.setConfiguration(conf);
        appMasterUgi = UserGroupInformation.getCurrentUser();
+       conf.setBoolean(TezConfiguration.TEZ_AM_NODE_BLACKLISTING_ENABLED, false);
+       conf.set(TezConfiguration.TEZ_HISTORY_LOGGING_SERVICE_CLASS,
+           TezConfiguration.TEZ_HISTORY_LOGGING_SERVICE_CLASS_DEFAULT);
     }
     conf.setBoolean(Dispatcher.DISPATCHER_EXIT_ON_ERROR_KEY, !isLocal);
     String strAppId = this.appAttemptID.getApplicationId().toString();
