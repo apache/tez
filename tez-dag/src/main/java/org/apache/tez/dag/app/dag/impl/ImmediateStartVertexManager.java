fcc74261a0d76d00580f4d0dca042a1ed014ccec
Rajesh Balamohan <rbalamohan@apache.org>
2014-09-12 04:09:21 +0530

    TEZ-1494. DAG hangs waiting for ShuffleManager.getNextInput() (Rajesh Balamohan)

--- tez-dag/src/main/java/org/apache/tez/dag/app/dag/impl/ImmediateStartVertexManager.java
+++ tez-dag/src/main/java/org/apache/tez/dag/app/dag/impl/ImmediateStartVertexManager.java
@@ -18,9 +18,11 @@
 
 package org.apache.tez.dag.app.dag.impl;
 
-import java.util.List;
-import java.util.Map;
-
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tez.dag.api.EdgeProperty;
 import org.apache.tez.dag.api.InputDescriptor;
 import org.apache.tez.dag.api.VertexManagerPlugin;
 import org.apache.tez.dag.api.VertexManagerPluginContext;
@@ -28,12 +30,28 @@ import org.apache.tez.dag.api.VertexManagerPluginContext.TaskWithLocationHint;
 import org.apache.tez.runtime.api.Event;
 import org.apache.tez.runtime.api.events.VertexManagerEvent;
 
-import com.google.common.collect.Lists;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Starts all tasks immediately on vertex start
  */
 public class ImmediateStartVertexManager extends VertexManagerPlugin {
+
+  private static final Log LOG = LogFactory.getLog(ImmediateStartVertexManager.class);
+
+  private final Map<String, SourceVertexInfo> srcVertexInfo = Maps.newHashMap();
+  private int managedTasks;
+  private boolean tasksScheduled = false;
+
+  class SourceVertexInfo {
+    EdgeProperty edgeProperty;
+    int numFinishedTasks;
+
+    SourceVertexInfo(EdgeProperty edgeProperty) {
+      this.edgeProperty = edgeProperty;
+    }
+  }
 
   public ImmediateStartVertexManager(VertexManagerPluginContext context) {
     super(context);
@@ -41,16 +59,72 @@ public class ImmediateStartVertexManager extends VertexManagerPlugin {
 
   @Override
   public void onVertexStarted(Map<String, List<Integer>> completions) {
-    int numTasks = getContext().getVertexNumTasks(getContext().getVertexName());
-    List<TaskWithLocationHint> scheduledTasks = Lists.newArrayListWithCapacity(numTasks);
-    for (int i=0; i<numTasks; ++i) {
-      scheduledTasks.add(new TaskWithLocationHint(new Integer(i), null));
+    managedTasks = getContext().getVertexNumTasks(getContext().getVertexName());
+    Map<String, EdgeProperty> edges = getContext().getInputVertexEdgeProperties();
+    for (Map.Entry<String, EdgeProperty> entry : edges.entrySet()) {
+      String srcVertex = entry.getKey();
+      EdgeProperty edgeProp = entry.getValue();
+      srcVertexInfo.put(srcVertex, new SourceVertexInfo(edgeProp));
     }
-    getContext().scheduleVertexTasks(scheduledTasks);
+
+    //handle completions
+    for (Map.Entry<String, List<Integer>> entry : completions.entrySet()) {
+      for (Integer task : entry.getValue()) {
+        handleSourceTaskFinished(entry.getKey(), task);
+      }
+    }
+    scheduleTasks();
+  }
+
+  private void handleSourceTaskFinished(String vertex, Integer taskId) {
+    SourceVertexInfo srcInfo = srcVertexInfo.get(vertex);
+    //Not mandatory to check for duplicate completions here
+    srcInfo.numFinishedTasks++;
+  }
+
+  private void scheduleTasks() {
+    if (!canScheduleTasks()) {
+      return;
+    }
+
+    List<TaskWithLocationHint> tasksToStart = Lists.newArrayListWithCapacity(managedTasks);
+    for (int i = 0; i < managedTasks; ++i) {
+      tasksToStart.add(new TaskWithLocationHint(new Integer(i), null));
+    }
+
+    if (!tasksToStart.isEmpty()) {
+      LOG.info("Starting " + tasksToStart.size() + " in " + getContext().getVertexName());
+      getContext().scheduleVertexTasks(tasksToStart);
+    }
+    tasksScheduled = true;
+  }
+
+  private boolean canScheduleTasks() {
+    //Check if at least 1 task is finished from each source vertex (in case of broadcast &
+    // one-to-one or custom)
+    for (Map.Entry<String, SourceVertexInfo> entry : srcVertexInfo.entrySet()) {
+      SourceVertexInfo srcVertexInfo = entry.getValue();
+      switch(srcVertexInfo.edgeProperty.getDataMovementType()) {
+      case ONE_TO_ONE:
+      case BROADCAST:
+      case CUSTOM:
+        if (srcVertexInfo.numFinishedTasks == 0) {
+          //do not schedule tasks until a task from source task is complete
+          return false;
+        }
+      default:
+        break;
+      }
+    }
+    return true;
   }
 
   @Override
   public void onSourceTaskCompleted(String srcVertexName, Integer attemptId) {
+    handleSourceTaskFinished(srcVertexName, attemptId);
+    if (!tasksScheduled) {
+      scheduleTasks();
+    }
   }
 
   @Override
