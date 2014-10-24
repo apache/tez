9e1eb5277dccfc16bebe07bab86351ff258bdd45
Siddharth Seth <sseth@apache.org>
2014-10-24 10:46:54 -0700

    TEZ-1542. Fix a Local Mode crash on concurrentModificationException. Contributed by Chen He.

--- tez-common/src/test/java/org/apache/tez/common/TestEnvironmentUpdateUtils.java
+++ tez-common/src/test/java/org/apache/tez/common/TestEnvironmentUpdateUtils.java
@@ -20,7 +20,19 @@ package org.apache.tez.common;
 
 import static org.junit.Assert.assertEquals;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TestEnvironmentUpdateUtils {
 
@@ -31,4 +43,54 @@ public class TestEnvironmentUpdateUtils {
     assertEquals("Environment was not set propertly", "test.value1", System.getenv("test.environment1"));
     assertEquals("Environment was not set propertly", "test.value2", System.getenv("test.environment2"));
   }
-}
+
+  @Test
+  public void testConcurrentRequests() throws InterruptedException {
+    int timeoutSecond = 5;
+    int concurThread = 10;
+    int exceptionCount = 0;
+    List<Callable<Object>> tasks = new ArrayList<Callable<Object>>();
+    List<ListenableFuture<Object>> pendingTasks = new ArrayList<ListenableFuture<Object>>();
+    final ExecutorService callbackExecutor = Executors.newFixedThreadPool(concurThread,
+        new ThreadFactoryBuilder().setDaemon(false).setNameFormat("CallbackExecutor").build());
+    ListeningExecutorService taskExecutorService =
+        MoreExecutors.listeningDecorator(callbackExecutor);
+    while(concurThread > 0){
+      ListenableFuture<Object> runningTaskFuture =
+          taskExecutorService.submit(new EnvironmentRequest());
+      pendingTasks.add(runningTaskFuture);
+      concurThread--;
+    }
+
+    //waiting for all threads submitted to thread pool
+    for (ListenableFuture<Object> future : pendingTasks) {
+     try {
+        future.get();
+      } catch (ExecutionException e) {
+        exceptionCount++;
+      }
+    }
+
+    //stop accepting new threads and shutdown threadpool
+    taskExecutorService.shutdown();
+    try {
+      if(!taskExecutorService.awaitTermination(timeoutSecond, TimeUnit.SECONDS)) {
+        taskExecutorService.shutdownNow();
+      }
+    } catch (InterruptedException ie) {
+      taskExecutorService.shutdownNow();
+    }
+
+    assertEquals(0, exceptionCount);
+  }
+
+  private class EnvironmentRequest implements Callable<Object> {
+
+    @Override
+    public Object call() throws Exception {
+      EnvironmentUpdateUtils.put("test.environment.concurrent"
+          +Thread.currentThread().getId(), "test.evironment.concurrent");
+      return null;
+    }
+  }
+ }
