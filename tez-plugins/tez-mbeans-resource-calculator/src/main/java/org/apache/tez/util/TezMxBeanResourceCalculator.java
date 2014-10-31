4d94652c817d83204ae06d618ade07f15fbed8d7
Rajesh Balamohan <rbalamohan@apache.org>
2014-10-31 08:12:21 +0530

    TEZ-1698. Cut down on ResourceCalculatorProcessTree overheads in Tez (Rajesh Balamohan & Gopal via Rajesh Balamohan)

---
+++ tez-plugins/tez-mbeans-resource-calculator/src/main/java/org/apache/tez/util/TezMxBeanResourceCalculator.java
@@ -0,0 +1,77 @@
+package org.apache.tez.util;
+
+import org.apache.hadoop.yarn.util.ResourceCalculatorProcessTree;
+
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Uses sun's MBeans to return process information.
+ */
+public class TezMxBeanResourceCalculator extends ResourceCalculatorProcessTree {
+
+  private final com.sun.management.OperatingSystemMXBean osBean;
+  private final Runtime runtime;
+  private final AtomicLong cumulativeCPU;
+
+  /**
+   * Create process-tree instance with specified root process.
+   * <p/>
+   * Subclass must override this.
+   *
+   * @param root process-tree root-process
+   */
+  public TezMxBeanResourceCalculator(String root) {
+    super(root);
+    runtime = Runtime.getRuntime();
+    osBean =
+        (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    cumulativeCPU = new AtomicLong();
+  }
+
+  @Override public void updateProcessTree() {
+    //nothing needs to be done as the data is read from OS mbeans.
+  }
+
+  @Override public String getProcessTreeDump() {
+    return "";
+  }
+
+  @Override public long getCumulativeVmem(int olderThanAge) {
+    return osBean.getCommittedVirtualMemorySize();
+  }
+
+  @Override public long getCumulativeRssmem(int olderThanAge) {
+    //Not supported directly (RSS ~= memory consumed by JVM from Xmx)
+    return runtime.totalMemory();
+  }
+
+  @Override public long getCumulativeCpuTime() {
+    //convert to milliseconds
+    return TimeUnit.MILLISECONDS.convert(cumulativeCPU.addAndGet(osBean.getProcessCpuTime()),
+        TimeUnit.MILLISECONDS);
+  }
+
+  @Override public boolean checkPidPgrpidForMatch() {
+    return true;
+  }
+}
---
+++ tez-plugins/tez-mbeans-resource-calculator/src/test/java/org/apache/tez/util/TestTezMxBeanResourceCalculator.java
@@ -0,0 +1,74 @@
+package org.apache.tez.util;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.timeline.TimelineEntity;
+import org.apache.hadoop.yarn.client.api.TimelineClient;
+import org.apache.hadoop.yarn.util.ResourceCalculatorProcessTree;
+import org.apache.tez.dag.api.TezConfiguration;
+import org.apache.tez.dag.app.AppContext;
+import org.apache.tez.dag.history.DAGHistoryEvent;
+import org.apache.tez.dag.history.events.DAGStartedEvent;
+import org.apache.tez.dag.records.TezDAGID;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class TestTezMxBeanResourceCalculator {
+
+  private ResourceCalculatorProcessTree resourceCalculator;
+
+  @Before
+  public void setup() throws Exception {
+    Configuration conf = new TezConfiguration();
+    conf.set(TezConfiguration.TEZ_TASK_RESOURCE_CALCULATOR_PROCESS_TREE_CLASS,
+        TezMxBeanResourceCalculator.class.getName());
+
+    Class<? extends ResourceCalculatorProcessTree> clazz = conf.getClass(
+        TezConfiguration.TEZ_TASK_RESOURCE_CALCULATOR_PROCESS_TREE_CLASS, null,
+        ResourceCalculatorProcessTree.class);
+    resourceCalculator = ResourceCalculatorProcessTree.getResourceCalculatorProcessTree(
+        System.getenv().get("JVM_PID"), clazz, conf);
+  }
+
+  @After
+  public void teardown() {
+    resourceCalculator = null;
+  }
+
+  @Test(timeout=5000)
+  public void testResourceCalculator() {
+    Assert.assertTrue(resourceCalculator instanceof TezMxBeanResourceCalculator);
+    Assert.assertTrue(resourceCalculator.getCumulativeCpuTime() > 0);
+    Assert.assertTrue(resourceCalculator.getCumulativeVmem() > 0);
+    Assert.assertTrue(resourceCalculator.getCumulativeRssmem() > 0);
+    Assert.assertTrue(resourceCalculator.getProcessTreeDump().equals(""));
+    Assert.assertTrue(resourceCalculator.checkPidPgrpidForMatch());
+  }
+
+}
