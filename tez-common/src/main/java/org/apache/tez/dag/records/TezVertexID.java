cb598513eff75edb1915e58418aae61c7d04589b
Jeff Zhang <zjffdu@apache.org>
2015-07-06 09:55:01 +0800

    TEZ-2588. Improper argument name (Gabor via zjffdu)

--- tez-common/src/main/java/org/apache/tez/dag/records/TezVertexID.java
+++ tez-common/src/main/java/org/apache/tez/dag/records/TezVertexID.java
@@ -156,9 +156,9 @@ public class TezVertexID extends TezID {
     return dagId.hashCode() * 530017 + id;
   }
 
-  public static TezVertexID fromString(String taskIdStr) {
+  public static TezVertexID fromString(String vertexIdStr) {
     try {
-      String[] split = taskIdStr.split("_");
+      String[] split = vertexIdStr.split("_");
       String rmId = split[1];
       int appId = TezDAGID.tezAppIdFormat.get().parse(split[2]).intValue();
       int dagId = TezDAGID.tezDagIdFormat.get().parse(split[3]).intValue();
