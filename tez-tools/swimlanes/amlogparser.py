be5b1912644efda7b198ce1646b4378c58ddf992
Gopal V <gopalv@apache.org>
2015-07-16 15:56:12 -0700

    TEZ-2610: yarn-swimlanes for DAGs that use containers of a previous DAG (gopalv)

--- tez-tools/swimlanes/amlogparser.py
+++ tez-tools/swimlanes/amlogparser.py
@@ -72,6 +72,16 @@ class AppMaster(object):
 		self.dags = None
 	def __repr__(self):
 		return "[%s started at %d]" % (self.name, self.zero)
+
+class DummyAppMaster(object):
+	""" magic of duck typing """
+	def __init__(self, dag):
+		self.raw = None
+		self.kvs = {}
+		self.name = "Appmaster for %s" % dag.name
+		self.zero = dag.start
+		self.containers = None
+		self.dags = None
 	
 class Container(object):
 	def __init__(self, raw):
@@ -84,6 +94,17 @@ class Container(object):
 		self.node =""
 	def __repr__(self):
 		return "[%s start=%d]" % (self.name, self.start)
+
+class DummyContainer(object):
+	def __init__(self, attempt):
+		self.raw = None
+		self.kvs = {}
+		self.name = attempt.container
+		self.status = 0
+		self.start = attempt.start
+		self.stop = -1
+		self.status = 0
+		self.node = None
 
 class DAG(object):
 	def __init__(self, raw):
@@ -200,8 +221,14 @@ class AMLog(object):
 		for d in dags:
 			d.structure(vertexes)
 		for a in attempts:
-			c = containers[a.container]
-			c.node = a.node
+			if containers.has_key(a.container):
+				c = containers[a.container]
+				c.node = a.node
+			else:
+				c = DummyContainer(a)
+				containers[a.container] = c
+		if not am:
+			am = DummyAppMaster(first(dags))
 		am.containers = containers
 		am.dags = dags
 		return am
@@ -245,7 +272,6 @@ class AMLog(object):
 	def parse(self, l):		
 		if(l.find("[HISTORY]") != -1):
 			m = self.MAIN_RE.match(l)
-			print(m);
 			ts = m.group("ts")
 			dag = m.group("dag")
 			event = m.group("event")
