3466f77b9f7ab697271c03bc9a3d496854f83f9e
Sreenath Somarajapuram <sree@apache.org>
2016-04-20 11:00:03 +0530

    TEZ-3146. Tez UI 2: CSS & JS assets in the UI must be minified (sree)

--- tez-ui2/src/main/webapp/ember-cli-build.js
+++ tez-ui2/src/main/webapp/ember-cli-build.js
@@ -24,16 +24,21 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
+  var isProd = EmberApp.env() === 'production';
   var app = new EmberApp(defaults, {
     storeConfigInMeta: false,
     minifyCSS: {
-      enabled: false
+      enabled: isProd
+    },
+    minifyJS: {
+      // Will be minified by wro4j-maven-plugin for performance
+      enabled: false,
     },
     fingerprint: {
       enabled: false
     },
-    minifyJS: {
-      enabled: false
+    sourcemaps: {
+      enabled: !isProd
     }
   });
 
