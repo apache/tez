4389ce8cf368c913757a95788c9c792b2a71f37f
Hitesh Shah <hitesh@apache.org>
2016-04-26 13:33:05 -0700

    TEZ-3232. Disable randomFailingInputs in testFaulttolerance to unblock other tests. (hitesh)

--- tez-tests/src/test/java/org/apache/tez/test/TestFaultTolerance.java
+++ tez-tests/src/test/java/org/apache/tez/test/TestFaultTolerance.java
@@ -49,6 +49,7 @@ import org.apache.tez.test.dag.ThreeLevelsFailingDAG;
 import org.apache.tez.test.dag.TwoLevelsFailingDAG;
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -754,6 +755,7 @@ public class TestFaultTolerance {
     runDAGAndVerify(dag, DAGStatus.State.SUCCEEDED);
   }
   
+  @Ignore
   @Test (timeout=240000)
   public void testRandomFailingInputs() throws Exception {
     Configuration testConf = new Configuration(false);
