b8ff9417eda8885bdfb19cbe4a3d758ee9a23ec1
Sreenath Somarajapuram <sree@apache.org>
2016-08-04 09:20:42 +0530

    TEZ-3398. Tez UI: Bread crumb link to Application from Application details dag/configuration tab is broken (sree)

--- tez-ui/src/main/webapp/tests/unit/controllers/app-test.js
+++ tez-ui/src/main/webapp/tests/unit/controllers/app-test.js
@@ -37,3 +37,23 @@ test('Basic creation test', function(assert) {
 
   assert.equal(controller.tabs.length, 3);
 });
+
+test('breadcrumbs test', function(assert) {
+  let appID = 123,
+  appName = "app123",
+  controller = this.subject({
+    send: Ember.K,
+    initVisibleColumns: Ember.K,
+    model: {
+      app: {
+        name: appName
+      },
+      appID: appID
+    }
+  });
+
+  assert.equal(controller.get("breadcrumbs.length"), 1);
+  assert.equal(controller.get("breadcrumbs.0.text"), `Application [ ${appName} ]`);
+  assert.equal(controller.get("breadcrumbs.0.routeName"), 'app.index');
+  assert.equal(controller.get("breadcrumbs.0.model"), appID);
+});
