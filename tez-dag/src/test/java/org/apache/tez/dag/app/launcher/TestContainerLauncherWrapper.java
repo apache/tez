1136629ccc4eeab7bd20930c235c7fd42be3dd59
Jonathan Eagles <jeagles@yahoo-inc.com>
2016-12-15 16:06:43 -0600

    TEZ-3557. TEZ-3362 causes TestContainerLauncherWrapper#testDelegation to fail (Kuhu Shukla via jeagles)

--- tez-dag/src/test/java/org/apache/tez/dag/app/launcher/TestContainerLauncherWrapper.java
+++ tez-dag/src/test/java/org/apache/tez/dag/app/launcher/TestContainerLauncherWrapper.java
@@ -24,7 +24,7 @@ public class TestContainerLauncherWrapper {
   @Test(timeout = 5000)
   public void testDelegation() throws Exception {
     PluginWrapperTestHelpers.testDelegation(ContainerLauncherWrapper.class, ContainerLauncher.class,
-        Sets.newHashSet("getContainerLauncher"));
+        Sets.newHashSet("getContainerLauncher", "dagComplete"));
   }
 
 }
