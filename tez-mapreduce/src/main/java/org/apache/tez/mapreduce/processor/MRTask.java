81dacf58e08393e6dda32945c1a36b8168184d58
Siddharth Seth <sseth@apache.org>
2017-01-15 16:41:44 -0800

    TEZ-3443. Remove a repeated/unused method from MRTask. Contributed by darion yaphet

--- tez-mapreduce/src/main/java/org/apache/tez/mapreduce/processor/MRTask.java
+++ tez-mapreduce/src/main/java/org/apache/tez/mapreduce/processor/MRTask.java
@@ -345,28 +345,6 @@ public abstract class MRTask extends AbstractLogicalIOProcessor {
     return this.jobConf;
   }
 
-  /**
-   * Gets a handle to the Statistics instance based on the scheme associated
-   * with path.
-   *
-   * @param path the path.
-   * @param conf the configuration to extract the scheme from if not part of
-   *   the path.
-   * @return a Statistics instance, or null if none is found for the scheme.
-   */
-  @Private
-  public static List<Statistics> getFsStatistics(Path path, Configuration conf) throws IOException {
-    List<Statistics> matchedStats = new ArrayList<FileSystem.Statistics>();
-    path = path.getFileSystem(conf).makeQualified(path);
-    String scheme = path.toUri().getScheme();
-    for (Statistics stats : FileSystem.getAllStatistics()) {
-      if (stats.getScheme().equals(scheme)) {
-        matchedStats.add(stats);
-      }
-    }
-    return matchedStats;
-  }
-
   @Private
   public synchronized String getOutputName() {
     return "part-" + NUMBER_FORMAT.format(taskAttemptId.getTaskID().getId());
