a5ffdea62cd6afc4f4a4e54dc20ff93df10b641f
Jonathan Eagles <jeagles@yahoo-inc.com>
2017-03-06 09:42:30 -0600

    TEZ-3648. IFile.Write#close has an extra output stream flush

--- tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/sort/impl/IFile.java
+++ tez-runtime-library/src/main/java/org/apache/tez/runtime/library/common/sort/impl/IFile.java
@@ -214,9 +214,6 @@ public class IFile {
       //account for header bytes
       decompressedBytesWritten += HEADER.length;
 
-      //Flush the stream
-      out.flush();
-
       // Close the underlying stream iff we own it...
       if (ownOutputStream) {
         out.close();
@@ -226,7 +223,7 @@ public class IFile {
           compressedOut.finish();
           compressedOut.resetState();
         }
-        // Write the checksum
+        // Write the checksum and flush the buffer
         checksumOut.finish();
       }
       //header bytes are already included in rawOut
