bd2aebe793951d6a2b586a013869667d8b93b58f
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2021-02-04 12:26:53 +0100

    TEZ-4282: Possible NPE in LocalClient after TEZ-4236
    Change-Id: I557621777513758a32c6edf96cff55e23173e525

--- tez-dag/src/main/java/org/apache/tez/client/LocalClient.java
+++ tez-dag/src/main/java/org/apache/tez/client/LocalClient.java
@@ -157,7 +157,9 @@ public class LocalClient extends FrameworkClient {
   @Override
   public void killApplication(ApplicationId appId) {
     try {
-      clientHandler.shutdownAM();
+      if (clientHandler != null){
+        clientHandler.shutdownAM();
+      }
     } catch (TezException e) {
       throw new RuntimeException(e);
     }
@@ -403,7 +405,14 @@ public class LocalClient extends FrameworkClient {
   @Override
   public TezAppMasterStatus getAMStatus(Configuration configuration, ApplicationId appId,
       UserGroupInformation ugi) throws TezException, ServiceException, IOException {
-    return clientHandler.getTezAppMasterStatus();
+    if (isLocalWithoutNetwork) {
+      if (clientHandler == null) {
+        return TezAppMasterStatus.INITIALIZING;
+      }
+      return clientHandler.getTezAppMasterStatus();
+    } else {
+      return super.getAMStatus(configuration, appId, ugi);
+    }
   }
 
   @Override
@@ -438,7 +447,9 @@ public class LocalClient extends FrameworkClient {
   public boolean shutdownSession(Configuration configuration, ApplicationId sessionAppId,
       UserGroupInformation ugi) throws TezException, IOException, ServiceException {
     if (isLocalWithoutNetwork) {
-      clientHandler.shutdownAM();
+      if (clientHandler != null){
+        clientHandler.shutdownAM();
+      }
       return true;
     } else {
       return super.shutdownSession(configuration, sessionAppId, ugi);
