75f753e22ab489d0fc1ed9dd8f1f42dd97772e2a
Bodor Laszlo <bodorlaszlo0202@gmail.com>
2022-02-22 10:32:30 +0100

    TEZ-4388: Asynchttpclient can cause stuck TezChild processes (#189) (Laszlo Bodor reviewed by Rajesh Balamohan)

--- tez-runtime-library/src/main/java/org/apache/tez/http/async/netty/AsyncHttpConnection.java
+++ tez-runtime-library/src/main/java/org/apache/tez/http/async/netty/AsyncHttpConnection.java
@@ -36,6 +36,7 @@ import org.apache.tez.runtime.library.api.TezRuntimeConfiguration;
 import org.apache.tez.runtime.library.common.security.SecureShuffleUtils;
 import org.apache.tez.runtime.library.common.shuffle.orderedgrouped.ShuffleHeader;
 import org.apache.tez.util.StopWatch;
+import org.apache.tez.util.TezRuntimeShutdownHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -103,6 +104,16 @@ public class AsyncHttpConnection extends BaseHttpConnection {
               .build();
           DefaultAsyncHttpClientConfig config = builder.build();
           httpAsyncClient = new DefaultAsyncHttpClient(config);
+          TezRuntimeShutdownHandler.addShutdownTask(() -> {
+            try {
+              if (httpAsyncClient != null) {
+                httpAsyncClient.close();
+                httpAsyncClient = null;
+              }
+            } catch (IOException e) {
+              LOG.warn("Error while closing async client (this won't block shutdown)", e);
+            }
+          });
         }
       }
     }
