91d5ada02c1d3175cf3a92261ca567e1db574ca8
Mahesh Raju Somalaraju <maheshrajus@cloudera.com>
2022-12-13 17:47:21 +0530

    [TEZ-4463] Upgrade jquery-ui to 1.13.2 due to CVEs

--- tez-ui/src/main/webapp/ember-cli-build.js
+++ tez-ui/src/main/webapp/ember-cli-build.js
@@ -60,7 +60,7 @@ module.exports = function(defaults) {
 
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import('bower_components/jquery-ui/jquery-ui.js');
-  app.import('bower_components/jquery-ui/ui/tooltip.js');
+  app.import('bower_components/jquery-ui/jquery-ui.css');
 
   app.import('bower_components/more-js/dist/more.js');
 
