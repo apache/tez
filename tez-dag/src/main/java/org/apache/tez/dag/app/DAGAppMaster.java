2def3edfbc7d19b946f8b521ae046ed7288cedf1
Ayush Saxena <ayushsaxena@apache.org>
2024-08-16 14:54:42 +0530

    Test CI Run.

--- tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
+++ tez-dag/src/main/java/org/apache/tez/dag/app/DAGAppMaster.java
@@ -1762,6 +1762,7 @@ public class DAGAppMaster extends AbstractService {
       Throwable dependencyError = dependency.getFailureCause();
       if (dependencyError != null) {
         synchronized(this) {
+          // test
           dependenciesFailed = true;
           if(LOG.isDebugEnabled()) {
             LOG.debug("Service: " + service.getName() + " will fail to start"
